01d530e8aba7213346a76327d5bf1f87b4cc9dc7
Guilherme Faccin <gfaccin27@gmail.com>
2025-04-14 16:19:57 -0300

    fix: overflow-x responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { Footer } from "@/components/footer"
 
 export default function Home() {
   return (
-    <main>
+    <main className="overflow-x-hidden">
       <Navbar />
       <HeroSection />
       <ServicesSection />
